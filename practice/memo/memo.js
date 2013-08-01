2f39ccf8f2c7f778c09900722ae176492db43fc9
hariganet <hariganet.sandbox@gmail.com>
2013-08-02 00:53:53 +0900

    memo can be moved

--- practice/memo/memo.js
+++ practice/memo/memo.js
@@ -4,15 +4,31 @@ var offsetY = 0;
 
 //ドラッグ
 function dragMemo(event){
+  //付箋紙のIDを格納
+  event.dataTransfer.setData("text", event.target.id);
 
+  //ドラッグした付箋紙の取得
+  var memoElement = document.getElementById(event.target.id);
+  //ドラッグ座標と付箋紙頂点のオフセットをセット
+  offsetX = event.clientX - memoElement.offsetLeft;
+  offsetY = event.clientY - memoElement.offsetTop;
 }
 
 //ドロップ
 function dropMemo(event){
+  //格納されたIDを取り出す
+  var id = event.dataTransfer.getData("text");
 
+  //ドラッグした付箋紙の取得
+  var memoElement = document.getElementById(id);
+
+  //付箋紙の座標をドロップした座標にセット
+  memoElement.style.left = event.clientX - offsetX + "px";
+  memoElement.style.top = event.clientY - offsetY + "px";
 }
 
 //ドラッグ中
 function dragOverMemo(event){
-
+  //通常のドラッグの動作を禁止
+  event.preventDefault();
 }
