2285b297c6963b691a1347a973fd1a475a5528e4
hariganet <hariganet.sandbox@gmail.com>
2013-08-02 00:33:13 +0900

    first commit memo

---
+++ practice/memo/memo.js
@@ -0,0 +1,18 @@
+//ドラッグ座標と付箋紙頂点のオフセット
+var offsetX = 0;
+var offsetY = 0;
+
+//ドラッグ
+function dragMemo(event){
+
+}
+
+//ドロップ
+function dropMemo(event){
+
+}
+
+//ドラッグ中
+function dragOverMemo(event){
+
+}
