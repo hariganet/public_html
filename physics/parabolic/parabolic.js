8ae68cb4a949f90743752a6e9eaef079c78f0888
hariganet <hariganet.sandbox@gmail.com>
2013-08-15 02:05:51 +0900

    change to object-oriented noww

--- physics/parabolic/parabolic.js
+++ physics/parabolic/parabolic.js
@@ -2,92 +2,117 @@ var canvas;
 var context;
 var timer;
 
-var vx, vy;  //速度
-var x, y;   //ボールの中心
-var g = 9.8;  //重力加速度
+var g = 9.8;
 var t = 0.2;  //時間
-var e = -0.8; //はね返り係数
+var e = -0.9; //はね返り係数
+
+var ballCurrentId = 0;
+
+var ballArray = new Array();
+
+//Ball Class
+function Ball(id, vx, vy, x, y){
+
+  this.id = id;
+  this.vx = vx;
+  this.vy = vy;
+  this.x = x;
+  this.y = y;
+
+}
+
+function addBall(){
+  //new memo
+  var ball = new Ball(ballCurrentId, 10, -100, 15, canvas.height-15);
+
+  ballArray[ball.id] = ball;
+
+  ballCurrentId++;
+}
 
 function init(){
   //描画コンテキストを取得
   canvas = document.getElementById("canvas");
   context = canvas.getContext("2d");
 
-  //ボールの初速度
-  vx = 10;
-  vy = -100;
-
-  //ボールの初期位置
-  x = 15;
-  y = canvas.height;
+  //add ball
+  addBall();
   //最初のボールを描画
-  context.arc(x, y, 10, 0, 360*Math.PI/180);
+  //context.arc(x, y, 15, 0, 360*Math.PI/180);
 
   //タイマースタート
   timer = setInterval(draw, 20);
 }
 
+
 function draw(){
-  //x方向（等速度運動）
-  x = x + vx;
-  //y方向（等加速度運動）  
-  vy = vy + g * t;
-  y = y + vy * t + 0.5 * g * t * t;
+  
+  g = 9.8;  //重力加速度
+  var i;
+  for(i=0; i<ballArray.length; ++i){
+    //x方向（等速度運動）
+    ballArray[i].x = ballArray[i].x + ballArray[i].vx;
+    //y方向（等加速度運動）  
+    ballArray[i].vy = ballArray[i].vy + g * t;
+    ballArray[i].y = ballArray[i].y + ballArray[i].vy * t + 0.5 * g * t * t;
+    
 
-  //地面にぶつかったとき
-  if(y > canvas.height - 15){
-    y = canvas.height - 15;
-    vy = vy * e;
-  }
+    //地面にぶつかったとき
+    if(ballArray[i].y > canvas.height - 15){
+      ballArray[i].y = canvas.height - 15;
+      ballArray[i].vy = ballArray[i].vy * e;
+    }
 
-  //壁にぶつかったとき
-  if(x > canvas.width - 15){
-    x = canvas.width - 15;
-    vx = vx * e;
-  }else if(x < 15){
-    x = 15;
-    vx = vx * e;
-  }
+    //壁にぶつかったとき
+    if(ballArray[i].x > canvas.width - 15){
+      ballArray[i].x = canvas.width - 15;
+      ballArray[i].vx = ballArray[i].vx * e;
+    }else if(ballArray[i].x < 15){
+      ballArray[i].x = 15;
+      ballArray[i].vx = ballArray[i].vx * e;
+    }
   
 
+    //ボールを描画
+    context.beginPath();
 
-  //キャンバスを初期化
+    var thr = 127;
+
+    var r = Math.floor(ballArray[i].x);
+    if(r < 0){
+      r = -r;
+    }
+    r = r % thr + thr;
+
+    var g = Math.floor(ballArray[i].y);
+    if(g < 0){
+      g = -g;
+    }
+    g = g % thr + thr;
+
+    var b = Math.floor(ballArray[i].y-ballArray[i].x);
+    if(b < 0){
+      b = -b;
+    } 
+    b = b %  thr + thr;
+    context.fillStyle = "rgb(" + r + ", " + g + ", " + b + ")";
+    context.arc(ballArray[i].x, ballArray[i].y, 15, 0, 360*Math.PI/180);
+    context.arc(ballArray[i].x, ballArray[i].y, 15, 0, 360*Math.PI/180);
+    context.fill();
+  }
+
+
+    //キャンバスを初期化
   var canvasColor = "rgba(0, 0, 0, 0.1)";
   context.fillStyle = canvasColor;
   context.rect(0, 0, 1200, 600);
   context.fill();
 
-  //ボールを描画
-  context.beginPath();
-
-  var thr = 127;
-
-  var rr = Math.floor(vx);
-  if(rr < 0){
-    rr = -rr;
-  }
-  rr = rr % thr + thr;
-
-  var gg = Math.floor(vy);
-  if(gg < 0){
-    gg = -gg;
-  }
-  gg = gg % thr + thr;
-
-  var bb = Math.floor(vy-vx);
-  if(bb < 0){
-    bb = -bb;
-  }
-  bb = bb %  thr + thr;
-
-  context.fillStyle = "rgb(" + rr + ", " + gg + ", " + bb + ")";
-  context.arc(x, y, 15, 0, 360*Math.PI/180);
-  context.fill();
 }
 
 function jump(){
   //上にジャンプ
-  vy = g * -3; 
+//  vy = g * -3; 
   //x方向に加速
-  vx = vx * 1.5;
+  //vx = vx * 1.5;
 }
