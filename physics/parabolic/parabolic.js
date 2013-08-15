e9ec86cdd8dde0242582c670b19ad13d81916919
hariganet <hariganet.sandbox@gmail.com>
2013-08-16 00:21:21 +0900

    ball is jumped at the bottom & e is random

--- physics/parabolic/parabolic.js
+++ physics/parabolic/parabolic.js
@@ -1,10 +1,11 @@
 var canvas;
 var context;
 var timer;
+var timerCheck;
 
 var gravity = 9.8;
 var t = 0.2;  //時間
-var e = -1; //はね返り係数
+var e = -0.8; //はね返り係数
 
 var ballCurrentId = 0;
 
@@ -20,6 +21,23 @@ function Ball(id, vx, vy, x, y){
   this.y = y;
 
 }
+
+Ball.prototype.verocity = function(){
+
+  this.vx = this.vx * 1.1;
+  this.vy = this.vy * 1.1;
+  
+  if(this.y > canvas.height - 20  &&  Math.abs(this.vx) >14){
+    if(this.vx > 0){
+      this.vx = 10;
+    }else{
+      this.vx = -10;
+    }
+    this.vy = - (50 + Math.floor( Math.random() * 50 ) );
+  }
+
+};
+
 
 function addBall(id, vx, vy, x, y){
 
@@ -43,6 +61,7 @@ function init(){
 
   //タイマースタート
   timer = setInterval(draw, 20);
+  timerCheck = setInterval(check, 20);
 }
 
 function setColor(color, num){
@@ -54,9 +73,13 @@ function setColor(color, num){
 }
 
 function draw(){
- 
+
+
   for(var i=0; i<ballArray.length; ++i){
 
+    //はね返り係数をボールごとにランダムに設定 0.8~0.99
+    e = -( 0.8 + Math.floor( Math.random() * 20 ) * 0.01 );
+    
     //ボールを描画
     context.beginPath();
     
@@ -64,8 +87,8 @@ function draw(){
     var green = setColor(green, ballArray[i].y);
     var blue = setColor(blue, ballArray[i].y - ballArray[i].x);
     
-    context.fillStyle = "rgb(" + red + ", " + green + ", " + blue + ")";
-    context.arc(ballArray[i].x, ballArray[i].y, 15, 0, 360*Math.PI/180);
+    //context.fillStyle = "rgb(" + red + ", " + green + ", " + blue + ")";
+    context.fillStyle = "rgb(0, " + green + ", " + blue + ")";
     context.arc(ballArray[i].x, ballArray[i].y, 15, 0, 360*Math.PI/180);
     context.fill();
  
@@ -95,12 +118,12 @@ function draw(){
     //キャンバスを初期化
   var canvasColor = "rgba(0, 0, 0, 0.1)";
   context.fillStyle = canvasColor;
-  context.rect(0, 0, 1200, 600);
+  context.rect(0, 0, canvas.width, canvas.height);
   context.fill();
 }
 
 function makeBall(event){
-  if(ballCurrentId > 10){
+  if(ballCurrentId >= 15){
     delete ballArray[0];
     ballCurrentId = 0;
   }
@@ -109,9 +132,29 @@ function makeBall(event){
 
 }
 
+function check(){
+  for(var i=0; i<ballArray.length; ++i){
+      for(var j=i+1; j<ballArray.length; ++j){
+
+          var x2 = (ballArray[i].x - ballArray[j].x) * (ballArray[i].x - ballArray[j].x);
+          var y2 = (ballArray[i].y - ballArray[j].y) * (ballArray[i].y - ballArray[j].y);
+          var distance = Math.sqrt( x2 + y2 );
+
+          if(distance < 10){
+            ballArray[i].verocity();
+            ballArray[j].verocity();
+            //context.fillStyle = "rgb(0, 255, 0)";
+            //context.fillRect(10, 10, 100, 100);
+            //addBall(ballCurrentId, 0, -50, ballArray[i].x, ballArray[i].y);
+          }
+      }
+  }
+}
+
 function jump(){
   //上にジャンプ
 //  vy = g * -3; 
   //x方向に加速
   //vx = vx * 1.5;
 }
+
