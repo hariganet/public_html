0c9a2c7eebc0cc9e8e3700a2b04e1d2b13443857
hariganet <hariganet.sandbox@gmail.com>
2013-11-15 01:24:36 +0900

    index.js in client side written

--- node_practice/index.js
+++ node_practice/index.js
@@ -1,3 +1,7 @@
+window.onload = init;
+
+document.onKeyDown = MV;
+
 var CurX = Math.floor( Math.random() * 500 );
 var CurY = Math.floor( Math.random() * 300 );
 
@@ -38,6 +42,130 @@ socket.on("message", function(msg){
 });
 
 socket.on("EPOS", function(data){
+    var Enemy = data.split(",");
+    EneMove(Enemy[0],Enemy[1],Enemy[2],Enemy[3]);
+    EX = Enemy[0];
+    EY = Enemy[1];   
+    EneX = Enemy[2];
+    EneY = Enemy[3]; 
+    document.getElementById("info" .innerHTML = data;   
+    });
+
+//鬼の描画処理
+function EneMove(EX,EY,EneX,EneY){
+
+    CTX.fillStyle = "rgb(128,128,128)";
+    CTX.strokeStyle = "rgb(128,128,128)";
+    CTX.beginPath();
+    CTX.arc(EneX, EneY, 50, 0, Math.PI*2, false);
+    CTX.fill();
     
-    
-    })
+    CTX.fillStyle = "rgb(255,0,0)";
+    CTX.strokeStyle = "rgb(255,0,0)";
+    CTX.beginPath();
+    CTX.arc(EX, EY, 20, 0, Math.PI*2, false);
+    CTX.fill();
+
+    EneX = EX;
+    EneY = EY;
+}
+
+var TMP;
+var CVS;
+var CTX;
+var STP = 5;
+
+function init(){
+    CVS = document.getElementById('cvs');
+    CTX = CVS.getContext('2d');
+
+    SDATA = CurX + "," + CurY;
+    socket.send(SDATA);
+
+    CTX.beginPath();
+    CTX.arc(CurX, CurY, 10, 0, Math.PI*2, false);
+    CTX.fill();
+
+    CTX.fillStyle = "rgb(255,0,0)";
+    CTX.strokeStyle = "rgb(255,0,0)";
+    CTX.beginPath();
+    CTX.arc(EX, EY, 20, 0, Math.PI*2, false);
+    CTX.fill();
+
+    CTX.fillStyle = "rgb(0,0,0)";
+    CTX.strokeStyle = "rgb(0,0,0)";
+}
+
+function MV(){
+
+    var K = event.KeyCode;
+
+    switch(K){
+      case 104:
+        TMP = "上";
+        PointMove(1);
+        break;
+      case 100:
+        TMP = "左";
+        PointMove(2);
+        break;
+      case 102:
+        TMP = "右";
+        PointMove(3);
+        break;
+      case 98:
+        TMP = "下";
+        PointMove(4);
+        break;
+      default:
+        TMP = "そのキーは使えません！";
+        break;
+    }
+    document.getElementById("info").innerHTML = TMP + "(" + K + ")";
+}
+
+function PointMove(P){
+
+    CTX.fillStyle = "rgb(128,128,128)";
+    CTX.strokeStyle = "rgb(128,128,128)";
+    CTX.beginPath();
+    CTX.arc(CurX, CurY, 15, 0, Math.PI*2, false);
+    CTX.fill();
+ 
+    switch(P){
+      case 1:
+        CurY = CurY - STP;
+        break;
+      case 2:
+        CurX = CurX - STP;
+        break;
+      case 3:
+        CurX = CurX + STP;
+      case 4:
+        CurY = CurY + STP;
+        break;
+    }
+
+    if(CurX > 500){ CurX = CurX -500; }
+    if(CurX < 0){  CurX = CurX + 500; }
+    if(CurY > 300){ CurY = CurY - 300;  }
+    if(CurY < 0){ CurY = CurY + 300;  }
+
+    EneMove(EneX, EneY);
+
+    var AH = CTX.isPointInPath(CurX, CurY);
+    document.getElementById("info2").innerHTML = AH;
+    if(AH){
+      alert("接触しました");
+    }
+
+    CTX.fillStyle = "rgb(0,0,0)";
+    CTX.strokeStyle = "rgb(0,0,0)";
+    CTX.beginPath();
+    CTX.arc(CurX, CurY, 10, 0, Math.PI*2, false);
+    CTX.fill();
+
+    SDATA = CurX + "," + CurY;
+    socket.send(SDATA);
+}
+
