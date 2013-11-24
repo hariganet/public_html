bfa2b041928c9b72084ca7e61d3440d668c74ade
hariganet <hariganet.sandbox@gmail.com>
2013-11-25 01:54:56 +0900

    fixing not view typeing now

--- websocket-chat/client.js
+++ websocket-chat/client.js
@@ -8,36 +8,40 @@ $(document).ready(function(){
       //イベント名'all'でメッセージをサーバに送信する
       socket.emit('all',{
         action: 'post',
-      user: $('#user').val(),
-      css: $('#css').val(),
-      text: $('#text').val()
-    });
+        user: $('#user').val(),
+        css: $('#css').val(),
+        text: $('#text').val()
+      });
 
-    //タイピング中というステータスをサーバに送信する 
-    }else{
-      //イベント名'other'でメッセージをサーバに送信する
-        socket.emit('others',{
-          action: 'typing',
+    // タイピング中というステータスをサーバに送信する 
+    } else {
+      // イベント名'others'でメッセージをサーバに送信する
+      socket.emit('others',{
+        action: 'typing',
         user: $('#user').val()
-     });
+      });
     }
   });
 
   //サーバからのイベント'msg'を受信する
   socket.on('msg', function(data){
+
+
+    alert(data.action);
+
     switch(data.action){
       case 'post':  //発言の描画
         $('<li></li>').text(data.user + ': ' + data.text)
-                  .attr('style', data.css)
-                  .appendTo('body');
-        break;
+    .attr('style', data.css)
+    .appendTo('body');
+  break;
       case 'typing':  //タイピング中ステータスの描画
-        $('#typing').text(data.user + 'さんがタイピング中です...');
-        clearTimeout(timer);
-        timer = setTimeout(function(){
-          $('#typing').empty();
-        }, 3000);
-        break;
+  $('#typing').text(data.user + 'さんがタイピング中です...');
+  clearTimeout(timer);
+  timer = setTimeout(function(){
+    $('#typing').empty();
+  }, 3000);
+  break;
     }
   });
 });
